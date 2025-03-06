0226340baf86959fb3073db3b064992b336cf13d
Jmabeleira <ja238826@fi365.ort.edu.uy>
2025-03-06 14:57:13 -0300

    feat: Splitted the app.js logic into index.js and route.js

--- Server/app.js
+++ Server/app.js
@@ -1,31 +1,10 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
+
 
 const server = http.createServer((req, res) => {
-    const url = req.url;
-    console.log(url);
-    const method = req.method;
-    if(url === "/main"){
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action ="/message" method="POST"><input type="text"><button type = "submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
 
-    if(url === "/message" && method === "POST"){
-        fs.writeFileSync('message.txt', 'DUMMY');
-        res.statusCode = 302;
-        res.setHeader('Location', '/main');
-        return res.end();
-
-    }
-        res.write('<html>');
-        res.write('<head><title>My First Page</title></head>');
-        res.write('<body><h1>404 Page not found</h1></body>');
-        res.write('</html>');
-        return res.end();
-    
 });
 
-server.listen(3000);
+
+export default server;

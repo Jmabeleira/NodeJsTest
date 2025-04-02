65b0d2689e3f4db79d85c1cfe25111f464962ed0
Jmabeleira <ja238826@fi365.ort.edu.uy>
2025-04-02 13:26:01 -0300

    ChapterModel

---
+++ Project2/Models/Product.js
@@ -0,0 +1,44 @@
+const products = [];
+const path = require("path");
+const fs = require("fs");
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      } else {
+        console.log(err);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll() {
+    const p = path.join(
+        path.dirname(require.main.filename),
+        "data",
+        "products.json"
+      );
+    fs.readFile(p,(err, fileContent) => {
+        if (err) {
+            return [];
+        }
+        return JSON.parse(fileContent);
+        }
+    )
+  }
+};
